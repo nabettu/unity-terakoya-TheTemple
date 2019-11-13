257a769c23439cc87e1203c6b9b3eb5066209eba
nabettu <tatsuaki.w@gmail.com>
2019-11-13 15:41:05 +0900

    initwq

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+  private const int MAX_ORB = 10;
+
+  public GameObject orbPrefab;
+  public GameObject canvasGame;
+  public GameObject textScore;
+
+  private int score = 0;
+  private int nextScore = 100;
+
+  void Start()
+  {
+    for (int i = 0; i < MAX_ORB; i++)
+    {
+      CreateOrb();
+    }
+    RefreshScoreText();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void CreateOrb()
+  {
+    GameObject orb = (GameObject)Instantiate(orbPrefab);
+    orb.transform.SetParent(canvasGame.transform, false);
+    orb.transform.localPosition = new Vector3(
+        UnityEngine.Random.Range(-300.0f, 300.0f),
+        UnityEngine.Random.Range(-140.0f, -500.0f),
+        0f
+    );
+  }
+
+  public void GetOrb()
+  {
+    score++;
+    RefreshScoreText();
+  }
+  void RefreshScoreText()
+  {
+    textScore.GetComponent<Text>().text = "徳： " + score + " / " + nextScore;
+  }
+}
---
+++ Assets/Scripts/OrbManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbManager : MonoBehaviour
+{
+
+  private GameObject gameManager;
+  // Start is called before the first frame update
+  void Start()
+  {
+    gameManager = GameObject.Find("GameManager");
+    // Debug.Log(gameManager.GetComponent<GameManager>().GetOrb());
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  public void TouchOrb()
+  {
+    if (Input.GetMouseButton(0) == false)
+    {
+      return;
+    }
+    gameManager.GetComponent<GameManager>().GetOrb();
+    Destroy(this.gameObject);
+  }
+
+}
